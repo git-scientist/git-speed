caab8e76b5dc1aecef719ceb70237d83c312e260
Daniel Tipping <daniel@oldreliable.tech>
2020-04-29 10:26:58 +0100

    Add optional Bash prompt install
    Adds current git branch and clean status to the prompt.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git-speed
+
 Git aliases to speed you up.
+
+See https://www.gitscientist.com for more.
--- git_speed/installer.py
+++ git_speed/installer.py
@@ -33,3 +33,48 @@ if [ -f {ALIAS_INSTALL_PATH} ]; then
 fi
 """
         )
+
+    cli.success("Installed aliases.")
+
+    install_prompt = typer.confirm(
+        "Would you like to add your current Git branch to your Bash prompt?"
+    )
+
+    if install_prompt:
+        with open(BASHRC, "a") as f:
+            f.write(
+                r"""
+# Added by git-speed
+if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+    # We have color support; assume it's compliant with Ecma-48
+    # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+    # a case would tend to support setf rather than setaf.)
+    color_prompt=yes
+else
+    color_prompt=
+fi
+
+# Added by git-speed
+parse_git_dirty() {
+    [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit"* ]] && echo "~"
+}
+
+# Added by git-speed
+parse_git_branch() {
+    echo "($(git rev-parse --abbrev-ref HEAD)$(parse_git_dirty))"
+}
+
+# Added by git-speed
+if [ "$color_prompt" = yes ]; then
+    user_style='01;32m'
+    dir_style='01;34m'
+    git_style='01;33m'
+    term_char='$'
+    PS1="${debian_chroot:+($debian_chroot)}\[\033[$user_style\]\u@\h\[\033[00m\]:\[\\033[$dir_style\]\w\[\033[$git_style\]\$(parse_git_branch)\[\033[00m\]$term_char "
+else
+    PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch)\$ "
+fi
+unset color_prompt
+"""
+            )
+        cli.success("Updated Bash prompt.")
